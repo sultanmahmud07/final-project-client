34655aa93b7172b4c6b81b8b38d9d496af939b5c
sultanmahmud07 <asadkhan01862@gmail.com>
2022-11-26 13:43:28 +0600

    google sigUp is done

--- src/Pages/Shared/GoogleLogin/GoogleLogin.js
+++ src/Pages/Shared/GoogleLogin/GoogleLogin.js
@@ -6,25 +6,57 @@ import useToken from '../../../hooks/UseToken';
 
 const GoogleLogin = () => {
   const {googleSignIn} =useContext(AuthContext);
-  // const [googlUserEmail, setGoogleUserEmail] = useState('');
-  // const [token] =useToken(googlUserEmail);
+  const [googlUserEmail, setGoogleUserEmail] = useState('');
+  const [token] =useToken(googlUserEmail);
   const location = useLocation();
   const navigate = useNavigate();
 
   const from = location.state?.from?.pathname || '/';
+
+  if(token){
+    navigate(from, {replace: true});
+  }
   const handleGoogle = () => {
     googleSignIn()
     .then(result => {
       const user = result.user;
       console.log(user);
-      // setGoogleUserEmail()
-      navigate(from, {replace: true});
+      // setGoogleUserEmail(user?.email)
+      const name =user.displayName
+      const email =user.email
+      const role = 'buyer'
+      saveUser(name, email, role)
+      
       toast('Google Login Successfully')
     })
     .catch(error => {
       console.log(error)
     })
   }
+
+
+
+  // Post user information in database >>>>>>>
+  const  saveUser = (name, email, role) => {
+    const  user = {name, email, role};
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log('save user', data);
+      setGoogleUserEmail(email)
+      // getUserToken(email)
+      
+  
+    })
+  }
+
+
   return (
     <div>
       <button onClick={handleGoogle} className="btn btn-outline btn-primary w-full">CONTINUE WITH GOOGLE</button>
